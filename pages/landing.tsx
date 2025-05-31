eee0da11bcdc4291c70c1177e94f9f8116bcb4f4
Ahmed Mohamed <ahmedabdelmoeen99@gmail.com>
2025-05-31 23:58:52 +0200

    Add Button component with dynamic styles and titles

--- pages/landing.tsx
+++ pages/landing.tsx
@@ -1,4 +1,5 @@
 import Card from "@/components/Card";
+import Button from "@/components/Button";
 
 const Landing: React.FC =  () => {
   return (
@@ -6,6 +7,9 @@ const Landing: React.FC =  () => {
       <h1 className="text-xl font-extralight">Landing Page</h1>
       <Card />
       <Card />
+      <Button title="Small Rounded" styles="text-sm rounded-sm" />
+<Button title="Medium Rounded" styles="text-md rounded-md mt-2" />
+<Button title="Large Rounded Full" styles="text-lg rounded-full mt-2" />
     </div>
   )
 }
